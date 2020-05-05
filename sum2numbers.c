4bdc1b656007b972561a7e60283a4cfda930f37f
clanert27 <64724214+clanert27@users.noreply.github.com>
2020-05-05 12:44:52 +0530

    FUNCTIONS
    Ssum of 2 numbers

---
+++ sum2numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+sum()
+{
+    int a=0,b=0;
+    printf("enter two values = ");
+    scanf("%d%d",&a,&b);
+    printf(" the sum of these 2 numbers is %d",a+b);
+}
+int main()
+{
+   sum() ;
+}
